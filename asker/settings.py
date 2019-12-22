d9ac1b11ce70f0003a13337d6062d0d569047bc2
dankiy <iorsadg@gmail.com>
2019-12-23 01:02:00 +0300

    DJANGO_DEBUG as env

--- asker/settings.py
+++ asker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DJANGO_DEBUG']
 
 ALLOWED_HOSTS = ['nameless-badlands-74513.herokuapp.com','127.0.0.1']
 
