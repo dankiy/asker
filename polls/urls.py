6aecfec9c9d1ac6d7de0e957246fa79535b294fb
Danil Kulminskiy <iorsadg@gmail.com>
2019-12-23 18:58:51 +0300

    fixing urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.QuestionCreate.as_view(), name='create'),
     path('', views.IndexView.as_view(), name='index'),
+    path('create/', views.QuestionCreate.as_view(), name='create'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
